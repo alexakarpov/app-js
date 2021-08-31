1ff113bce23ed0fcf3f98a662a5123630339f8ac
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-31 09:43:08 -0400

    move index to public, and app.js to scripts inside of public, and manually compile JSX into plain old JS

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log('Abbe');
-
-// JSX - Javascript XML new syntax 
-
-var template = <p>This is JSX from app.js</p>;
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
