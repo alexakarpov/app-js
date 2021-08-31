40501f99ed5e828ed8776ae74549718878470e12
Ludzos <88338317+Ludzos@users.noreply.github.com>
2021-08-31 07:57:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Abbe');
+
+// JSX - Javascript XML new syntax 
+
+var template = <p>This is JSX from app.js</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
